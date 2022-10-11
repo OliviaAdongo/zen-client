6dd4ba6f7b3945305a166b36fea7129737315c23
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-11 21:54:16 +0300

    Sig up and login edited

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,18 +2,31 @@ import React from 'react'
 import './Login.css'
 function Login() {
   return (
-    <form> 
-    <div class="mb-3">
-      <label for="formGroupExampleInput" class="form-label">Username:</label>
-      <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter your username"/>
+    <div className='login'>
+   <div className='form'>
+        <form class="needs-validation"> 
+        <h1>SIGN IN</h1>
+          <div className='form-group'>
+            <label for="text" class="form-label">Username:</label>
+            <input type="text" class="form-control" id="input" placeholder="Enter your username"/>
+            <div class="invalid-feedback">
+                Please enter your username again
+            </div>
+          </div>
+            <div className='form-group'>
+              <label for="password" class="form-label">Password:</label>
+              <input type="password" class="form-control" id="password"/>
+              <div class="invalid-feedback">
+                Please enter your password again
+            </div>
+            </div>
+            
+            <button type="submit" class="btn w-100">Submit</button>
+          </form>
+   </div> 
+  
     </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password:</label>
-        <input type="password" class="form-control" id="exampleInputPassword1"/>
-      </div>
-      
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    
   )
 }
 
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import './Signup.css'
+
 function Signup() {
   return (
     <div className='signup'>
-        <form>
-          <label>Username: </label> <br/>
-          <input type="text" className='name' id="formGroupExampleInput" placeholder="Enter username"/><br/>
-          <label>Password: </label><br/>
-          <input type="password" className='name' id="formGroupExampleInput" placeholder="Enter password"/><br/>
-          <button type="submit" class="btn btn-primary">Submit</button><br/>
-        </form>
-    </div>    
+   <div className='form'>
+        <form class="needs-validation"> 
+        <h1>REGISTER</h1>
+          <div className='form-group'>
+            <label for="email" class="form-label">Email:</label>
+            <input type="email" class="form-control" id="input" placeholder="Enter your email"/>
+            <div class="invalid-feedback">
+                Please enter valid email
+            </div>
+          </div>
+          <div className='form-group'>
+            <label for="text" class="form-label">Username:</label>
+            <input type="text" class="form-control" id="input" placeholder="Enter your username"/>
+  
+          </div>
+            <div className='form-group'>
+              <label for="password" class="form-label">Password:</label>
+              <input type="password" class="form-control" id="password"/>
+            </div>
+            
+            <button type="submit" class="btn w-100">Submit</button>
+          </form>
+   </div> 
+  
+    </div>
   )
 }
 
